5ef412a2dc2360f34de4fffe09746b57eb76219d
brian heise <bnheise@gmail.com>
2021-11-17 08:03:06 +0900

    moved global object typedef to globalObject folder

---
+++ globalObject/Liferay.d.ts
@@ -0,0 +1,24 @@
+import { Address } from "./Address";
+import { AUI } from "./AUI";
+import { Browser } from "./Browser";
+import { BrowserSelectors } from "./BrowserSelectors";
+import { Data } from "./Data";
+import { Dependency } from "./Dependency";
+import { DOMTaskRunner } from "./DOMTaskRunner";
+import { Language } from "./Language";
+
+export type Liferay = {
+  AUI: AUI;
+  Address: Address;
+  Alert: (...args: unknown[]) => unknown; // TODO: fix this typedef
+  BREAKPOINTS: { PHONE: number; TABLET: number };
+  Browser: Browser;
+  BrowserSelectors: BrowserSelectors;
+  DOMTaskRunner: DOMTaskRunner;
+  Data: Data;
+  Dependency: Dependency;
+  EXPLAIN_RESOLUTIONS: boolean;
+  EXPOSE_GLOBAL: boolean;
+  Form: () => void;
+  Language: Language;
+};
