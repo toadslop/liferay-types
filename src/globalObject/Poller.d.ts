3d27426ba6529e6e1b99595abc9319ec2378f214
brian heise <bnheise@gmail.com>
2021-11-20 15:31:17 +0900

    finished poller

--- src/globalObject/Poller.d.ts
+++ src/globalObject/Poller.d.ts
@@ -6,6 +6,17 @@ export type Poller = {
   getSendUrl: () => string;
   init: (options: InitPollerOptions) => void;
   isSupportsComet: () => boolean;
+  processResponse: (id: string, obj: ResponseObject) => void;
+  removeListener: (key: string) => void;
+  resume: () => void;
+  setCustomDelay: (delay: number) => void;
+  setDelay: (delay: number) => void;
+  setEncryptedUserId: (encryptedUserId: string | number) => void;
+  setSupportsComet: (supportsComet: boolean) => void;
+  setUrl: (url: string) => void;
+  submitRequest: (key: string, data: object, chunkId: string) => void;
+  suspend: () => void;
+  url: string;
 };
 
 export type Listener = (event: Event) => void;
@@ -13,3 +24,6 @@ export type InitPollerOptions = {
   encryptedUserId: string;
   supportsComet: boolean;
 };
+export interface ResponseObject {
+  responseText: string;
+}
